89205a4e9d78804ad675b0388283de7dfd454ad4
ReiDy <k022g0060@g.neec.ac.jp>
2023-10-23 12:59:12 +0900

    01_04_一応ラムダ式とstd::functionに

--- main.cpp
+++ main.cpp
@@ -4,22 +4,20 @@
 #include <Windows.h>
 #include <functional>
 
+typedef void (*PFunc)(int);
 
 enum class Dice {
-	Even,	// 偶数
-	Odd		// 奇数
+	Even = 1,	// 偶数
+	Odd = 2		// 奇数
 };
-
 enum class Answer {
-	Yes,	// はい
-	No		// いいえ
+	Yes = 1,	// はい
+	No = 2		// いいえ
 };
-
 void DispResult(int* s) {
 	printf_s("%d秒まって実行されたよ\n", *s);
 }
 
-typedef void (*PFunc)(int);
 void setTimeout(PFunc p, int second, int answer) {
 	Sleep(second * 1000);
 
@@ -27,49 +25,50 @@ void setTimeout(PFunc p, int second, int answer) {
 }
 
 int RundomDice() {
-	return 1 + rand() % 6;
+	return 1 + rand() % 2;
 }
 
 void DiceGame(int answer) {
-	int check = 0;
+	//int check = 0;
 	int diceNum = RundomDice();
 
 	// 出目が偶数なら0 奇数なら1
-	check = diceNum % 2;
+	//check = diceNum % 2;
+	//printf_s("サイコロの出目 : %d\n", diceNum);
+	//Sleep(1 * 1000);
 
-	printf_s("サイコロの出目 : %d\n", diceNum);
-	Sleep(1 * 1000);
-
-	if (check == int(Dice::Even)) {
+	if (diceNum == int(Dice::Even)) {
 		printf_s("出目は丁\n");
 	}
-	else if (check == int(Dice::Odd)) {
+	else if (diceNum == int(Dice::Odd)) {
 		printf_s("出目は半\n");
 	}
 
 	Sleep(1 * 1000);
 
-	if (answer == check) {
+	if (answer == diceNum) {
 		printf_s("おめでとう! あなたは運があるね");
 	}
-	else if (answer != check) {
+	else if (answer != diceNum) {
 		printf_s("残念！ あなたは運がない");
 	}
 }
+
 
 int main() {
 	srand((unsigned)time(nullptr));
 	int isEvenOrOdd = 0;
 	int isYesOrNo = int(Answer::No);
-	while (isYesOrNo == int(Answer::Yes)) {
-		printf_s("丁(偶数)か半(奇数)か\n丁なら0を、半なら1を押してください\n");
+
+	for (; isYesOrNo == int(Answer::No);) {
+		printf_s("丁(偶数)か半(奇数)か\n丁なら1を、半なら2を押してください\n");
 		scanf_s("%d", &isEvenOrOdd);
-		while (isEvenOrOdd < int(Dice::Even) || isEvenOrOdd > int(Dice::Odd)) {
+		for (; (isEvenOrOdd < int(Dice::Even) || isEvenOrOdd > int(Dice::Odd));) {
 			printf_s("範囲外の回答です。もう一度お願いします。\n");
 			scanf_s("%d", &isEvenOrOdd);
 
 		}
-		printf_s("ファイナルアンサー? はい : 0, いいえ : 1\n");
+		printf_s("ファイナルアンサー? はい : 1, いいえ : 2\n");
 		scanf_s("%d", &isYesOrNo);
 		for (; (isYesOrNo < int(Answer::Yes) || isYesOrNo > int(Answer::No));) {
 			printf_s("範囲外の回答です。もう一度お願いします。\n");
@@ -77,12 +76,12 @@ int main() {
 
 		}
 	}
+	std::function<void(int)> afunc = [=](int isEvenOrOdd) {
+		PFunc p;
+		p = DiceGame;
 
-	std::function<void(int)> allFunction = [](int i){};
-
-	PFunc p;
-	p = DiceGame;
-	setTimeout(p, 3, isEvenOrOdd);
-
+		setTimeout(p, 3, isEvenOrOdd);
+	};
+	afunc(isEvenOrOdd);
 	return 0;
 }
