b73ba7c4b40bdba1a24ade75bd0018b1c0b5cd9c
ReiDy <k022g0060@g.neec.ac.jp>
2023-10-23 10:32:40 +0900

    ダイスの出目->丁半ではなく、最初から1/2で出すよう調整中

--- main.cpp
+++ main.cpp
@@ -2,61 +2,61 @@
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
+#include <functional>
 
-typedef void (*PFunc)(int);
 
 enum class Dice {
 	Even,	// 偶数
 	Odd		// 奇数
 };
+
 enum class Answer {
 	Yes,	// はい
 	No		// いいえ
 };
+
 void DispResult(int* s) {
 	printf_s("%d秒まって実行されたよ\n", *s);
 }
 
-void setTimeout(PFunc p, int second, int answer) {
+int RundomDice() {
+	return 1 + rand() % 2;
+}
+
+void setTimeout(std::function<void(int)> *p, int second, int answer) {
 	Sleep(second * 1000);
 	
 	p(answer);
 }
 
-int RundomDice() {
-	return 1 + rand() % 6;
-}
-
 void DiceGame(int answer) {
-	int check = 0;
-	int diceNum = RundomDice();
-
-	// 出目が偶数なら0 奇数なら1
-	check = diceNum % 2;
-
-	printf_s("サイコロの出目 : %d\n", diceNum);
-	Sleep(1 * 1000);
-
-	if (check == int(Dice::Even)) {
-		printf_s("出目は丁\n");
-	}
-	else if (check == int(Dice::Odd)) {
-		printf_s("出目は半\n");
-	}
-
-	Sleep(1 * 1000);
-
-	if (answer == check) {
-		printf_s("おめでとう! あなたは運があるね");
-	}
-	else if (answer != check) {
-		printf_s("残念！ あなたは運がない");
-	}
 }
+
 int main() {
 	srand((unsigned)time(nullptr));
 	int isEvenOrOdd = 0;
 	int isYesOrNo = int(Answer::No);
+	std::function<void(int)> pFunc = [](int answer) {
+		int check = 0;
+		int diceNum = RundomDice();
+
+		if (check == int(Dice::Even)) {
+			printf_s("出目は丁\n");
+		}
+		else if (check == int(Dice::Odd)) {
+			printf_s("出目は半\n");
+		}
+
+		if (answer == check) {
+			printf_s("おめでとう! あなたは運があるね");
+		}
+		else if (answer != check) {
+			printf_s("残念！ あなたは運がない");
+		}
+
+	}
+
+
 
 	for (; isYesOrNo == int(Answer::No);) {
 		printf_s("丁(偶数)か半(奇数)か\n丁なら0を、半なら1を押してください\n");
@@ -76,9 +76,8 @@ int main() {
 	}
 
 
-	PFunc p;
-	p = DiceGame;
-	setTimeout(p, 3, isEvenOrOdd);
+	
+	setTimeout(&pFunc, 3, isEvenOrOdd);
 
 	return 0;
 }
