23aef9fdbb3e5df67d9651ebc5ce1dd7ce02be4a
ReiDy <k022g0060@g.neec.ac.jp>
2023-11-20 11:47:21 +0900

    ラムダのとこ修正

--- main.cpp
+++ main.cpp
@@ -4,39 +4,35 @@
 #include <Windows.h>
 #include <functional>
 
-typedef void (*PFunc)(int);
-
-std::function<void(int)> SetTimeOut(int second, PFunc p) {
+void SetTimeOut(std::function<void()> pFunc, int second) {
 	Sleep(second * 1000);
 
-	return [p](int answer) { p(answer); };
-}
-
-void DisplayDice(int answer) {
-	int diceNum = 1 + rand() % 2;
-
-	if (diceNum == 0) {
-		printf_s("出目は丁\n");
-	}
-	else if (diceNum == 1) {
-		printf_s("出目は半\n");
-	}
-
-	Sleep(1 * 1000);
-
-	if (answer == diceNum) {
-		printf_s("おめでとう! あなたは運があるね");
-	}
-	else if (answer != diceNum) {
-		printf_s("残念！ あなたは運がない");
-	}
+	pFunc();
 }
 
 int main() {
 	srand((unsigned)time(nullptr));
 	int isEvenOrOdd = 0;
-	int isYesOrNo = 1;
+	int isYesOrNo = 1;	
 
+	std::function<void()> diceGameResult = [&]() {
+		int diceNum = rand() % 2;
+
+		if (diceNum == 0) {
+			printf_s("出目は丁\n");
+		}
+		else if (diceNum == 1) {
+			printf_s("出目は半\n");
+		}
+
+		if (isEvenOrOdd == diceNum) {
+			printf_s("おめでとう! あなたは運があるね");
+		}
+		else if (isEvenOrOdd != diceNum) {
+			printf_s("残念！ あなたは運がない");
+		}
+	};
+	// 問います。
 	for (; isYesOrNo == 1;) {
 		printf_s("丁(偶数)か半(奇数)か\n丁なら0を、半なら1を押してください\n");
 		scanf_s("%d", &isEvenOrOdd);
@@ -53,10 +49,8 @@ int main() {
 
 		}
 	}
-	PFunc p;
-	p = DisplayDice;
-	auto diceGame = SetTimeOut(3, p);
 
-	diceGame(isEvenOrOdd);
+	SetTimeOut(diceGameResult, 3);
+
 	return 0;
 }
